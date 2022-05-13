81da3e0fe6f60f580920ccac508faf0a155b0b28
Lone <gyl19930522@163.com>
2022-05-13 21:10:17 +0800

    update

--- program/src/verifier/mock/miller_loop.rs
+++ program/src/verifier/mock/miller_loop.rs
@@ -45,7 +45,7 @@ impl MillerLoop {
     ) -> Result<(), ProgramError> {
         let pvk = proof_type.verifying_key();
 
-        const MAX_LOOP: usize = 3;
+        const MAX_LOOP: usize = 2;
         for _ in 0..MAX_LOOP {
             self.f.square_in_place();
 
@@ -73,6 +73,17 @@ impl MillerLoop {
                         q2.y = -q2.y;
         
                         // in Finalize
+                        let coeff = addition_step(&mut self.r, &q1);
+                        ell(&mut self.f, &coeff, &self.proof_a);
+                        ell(&mut self.f, &pvk.gamma_g2_neg_pc[self.coeff_index as usize], &self.prepared_input);
+                        ell(&mut self.f, &pvk.delta_g2_neg_pc[self.coeff_index as usize], &self.proof_c);
+                        self.coeff_index += 1;
+                
+                        let coeff = addition_step(&mut self.r, &q2);
+                        ell(&mut self.f, &coeff, &self.proof_a);
+                        ell(&mut self.f, &pvk.gamma_g2_neg_pc[self.coeff_index as usize], &self.prepared_input);
+                        ell(&mut self.f, &pvk.delta_g2_neg_pc[self.coeff_index as usize], &self.proof_c);
+
                         return Ok(());
                     } else {
                         continue;
@@ -106,200 +117,38 @@ impl MillerLoop {
     }
 }
 
-// #[derive(Clone, Default, BorshSerialize, BorshDeserialize)]
-// pub struct MillerLoopFinalize {
-//     pub step: u8,
-//     pub prepared_input: Pubkey, // G1Affine254
-//     pub proof_a: Pubkey, // G1Affine254
-//     pub proof_c: Pubkey, // G1Affine254
-//     pub q1: Pubkey, // G2Affine254
-//     pub q2: Pubkey, // G2Affine254
-//     pub r: Pubkey, // G2HomProjective254
-//     pub f: Pubkey, // Fqk254
-// }
+#[derive(Clone, BorshSerialize, BorshDeserialize)]
+pub struct MillerLoopFinalize {
+    pub step: u8,
+    pub prepared_input: G1Affine254, // G1Affine254
+    pub proof_a: G1Affine254, // G1Affine254
+    pub proof_c: G1Affine254, // G1Affine254
+    pub q1: G2Affine254, // G2Affine254
+    pub q2: G2Affine254, // G2Affine254
+    pub r: G2HomProjective254, // G2HomProjective254
+    pub f: Fqk254, // Fqk254
+}
 
-// impl MillerLoopFinalize {
-//     #[allow(clippy::too_many_arguments)]
-//     pub fn process_step_0(
-//         mut self,
-//         proof_a_ctx: &Context<ProofA>,
-//         f_ctx: &Context<Fqk254>,
-//         r_ctx: &Context<G2HomProjective254>,
-//         q1_ctx: &Context<G2Affine254>,
-//     ) -> Result<FSM, ProgramError> {
-//         if f_ctx.pubkey() != &self.f {
-//             msg!("f_ctx pubkey mismatch");
-//             return Err(MazeError::UnmatchedAccounts.into());
-//         }
-//         if r_ctx.pubkey() != &self.r {
-//             msg!("r_ctx pubkey mismatch");
-//             return Err(MazeError::UnmatchedAccounts.into());
-//         }
-//         if q1_ctx.pubkey() != &self.q1 {
-//             msg!("q1_ctx pubkey mismatch");
-//             return Err(MazeError::UnmatchedAccounts.into());
-//         }
-//         if proof_a_ctx.pubkey() != &self.proof_a {
-//             msg!("proof_a_ctx pubkey mismatch");
-//             return Err(MazeError::UnmatchedAccounts.into());
-//         }
+impl MillerLoopFinalize {
+    #[allow(clippy::too_many_arguments)]
+    pub fn process_step_0(
+        mut self,
+        proof_type: &OperationType,
+    ) -> Result<(), ProgramError> {
+        let pvk = proof_type.verifying_key();
+        let mut index = pvk.gamma_g2_neg_pc.len() - 2;
 
-//         let mut f = f_ctx.borrow_mut()?;
-//         let mut r = r_ctx.borrow_mut()?;
-//         let q1 = q1_ctx.take()?;
-//         let proof_a = proof_a_ctx.take()?;
+        let coeff = addition_step(&mut self.r, &self.q1);
+        ell(&mut self.f, &coeff, &self.proof_a);
+        ell(&mut self.f, &pvk.gamma_g2_neg_pc[index], &self.prepared_input);
+        ell(&mut self.f, &pvk.delta_g2_neg_pc[index], &self.proof_c);
+        index += 1;
 
-//         let coeff = addition_step(&mut r, &q1);
-//         ell(&mut f, &coeff, &proof_a);
+        let coeff = addition_step(&mut self.r, &self.q2);
+        ell(&mut self.f, &coeff, &self.proof_a);
+        ell(&mut self.f, &pvk.gamma_g2_neg_pc[index ], &self.prepared_input);
+        ell(&mut self.f, &pvk.delta_g2_neg_pc[index], &self.proof_c);
 
-//         self.step += 1;
-//         q1_ctx.erase()?;
-//         Ok(FSM::MillerLoopFinalize(self))
-//     }
-
-//     pub fn process_step_1(
-//         mut self,
-//         proof_type: &OperationType,
-//         prepared_input_ctx: &Context<G1Affine254>,
-//         f_ctx: &Context<Fqk254>,
-//     ) -> Result<FSM, ProgramError> {
-//         if f_ctx.pubkey() != &self.f {
-//             msg!("f_ctx pubkey mismatch");
-//             return Err(MazeError::UnmatchedAccounts.into());
-//         }
-//         if prepared_input_ctx.pubkey() != &self.prepared_input {
-//             msg!("prepared_input_ctx pubkey mismatch");
-//             return Err(MazeError::UnmatchedAccounts.into());
-//         }
-
-//         let mut f = f_ctx.borrow_mut()?;
-//         let prepared_input = prepared_input_ctx.take()?;
-
-//         let pvk = proof_type.verifying_key();
-//         let index = pvk.gamma_g2_neg_pc.len() - 2;
-//         ell(&mut f, &pvk.gamma_g2_neg_pc[index], &prepared_input);
-
-//         self.step += 1;
-//         Ok(FSM::MillerLoopFinalize(self))
-//     }
-
-//     pub fn process_step_2(
-//         mut self,
-//         proof_type: &OperationType,
-//         proof_c_ctx: &Context<ProofC>,
-//         f_ctx: &Context<Fqk254>,
-//     ) -> Result<FSM, ProgramError> {
-//         if f_ctx.pubkey() != &self.f {
-//             msg!("f_ctx pubkey mismatch");
-//             return Err(MazeError::UnmatchedAccounts.into());
-//         }
-//         if proof_c_ctx.pubkey() != &self.proof_c {
-//             msg!("proof_c_ctx pubkey mismatch");
-//             return Err(MazeError::UnmatchedAccounts.into());
-//         }
-
-//         let mut f = f_ctx.borrow_mut()?;
-//         let proof_c = proof_c_ctx.take()?;
-
-//         let pvk = proof_type.verifying_key();
-//         let index = pvk.delta_g2_neg_pc.len() - 2;
-//         ell(&mut f, &pvk.delta_g2_neg_pc[index], &proof_c);
-
-//         self.step += 1;
-//         Ok(FSM::MillerLoopFinalize(self))
-//     }
-
-//     pub fn process_step_3(
-//         mut self,
-//         proof_a_ctx: &Context<ProofA>,
-//         f_ctx: &Context<Fqk254>,
-//         r_ctx: &Context<G2HomProjective254>,
-//         q2_ctx: &Context<G2Affine254>,
-//     ) -> Result<FSM, ProgramError> {
-//         if f_ctx.pubkey() != &self.f {
-//             msg!("f_ctx pubkey mismatch");
-//             return Err(MazeError::UnmatchedAccounts.into());
-//         }
-//         if r_ctx.pubkey() != &self.r {
-//             msg!("r_ctx pubkey mismatch");
-//             return Err(MazeError::UnmatchedAccounts.into());
-//         }
-//         if q2_ctx.pubkey() != &self.q2 {
-//             msg!("q2_ctx pubkey mismatch");
-//             return Err(MazeError::UnmatchedAccounts.into());
-//         }
-//         if proof_a_ctx.pubkey() != &self.proof_a {
-//             msg!("proof_a_ctx pubkey mismatch");
-//             return Err(MazeError::UnmatchedAccounts.into());
-//         }
-
-//         let mut f = f_ctx.borrow_mut()?;
-//         let mut r = r_ctx.take()?;
-//         let q2 = q2_ctx.take()?;
-//         let proof_a = proof_a_ctx.take()?;
-
-//         let coeff = addition_step(&mut r, &q2);
-//         ell(&mut f, &coeff, &proof_a);
-
-//         self.step += 1;
-//         r_ctx.erase()?;
-//         q2_ctx.erase()?;
-//         proof_a_ctx.erase()?;
-//         Ok(FSM::MillerLoopFinalize(self))
-//     }
-
-//     pub fn process_step_4(
-//         mut self,
-//         proof_type: &OperationType,
-//         prepared_input_ctx: &Context<G1Affine254>,
-//         f_ctx: &Context<Fqk254>,
-//     ) -> Result<FSM, ProgramError> {
-//         if f_ctx.pubkey() != &self.f {
-//             msg!("f_ctx pubkey mismatch");
-//             return Err(MazeError::UnmatchedAccounts.into());
-//         }
-//         if prepared_input_ctx.pubkey() != &self.prepared_input {
-//             msg!("prepared_input_ctx pubkey mismatch");
-//             return Err(MazeError::UnmatchedAccounts.into());
-//         }
-
-//         let mut f = f_ctx.borrow_mut()?;
-//         let prepared_input = prepared_input_ctx.take()?;
-
-//         let pvk = proof_type.verifying_key();
-//         let index = pvk.gamma_g2_neg_pc.len() - 1;
-//         ell(&mut f, &pvk.gamma_g2_neg_pc[index], &prepared_input);
-
-//         self.step += 1;
-//         prepared_input_ctx.erase()?;
-//         Ok(FSM::MillerLoopFinalize(self))
-//     }
-
-//     pub fn process_step_5(
-//         self,
-//         proof_type: &OperationType,
-//         proof_c_ctx: &Context<ProofC>,
-//         f_ctx: &Context<Fqk254>,
-//     ) -> Result<FSM, ProgramError> {
-//         if f_ctx.pubkey() != &self.f {
-//             msg!("f_ctx pubkey mismatch");
-//             return Err(MazeError::UnmatchedAccounts.into());
-//         }
-//         if proof_c_ctx.pubkey() != &self.proof_c {
-//             msg!("proof_c_ctx pubkey mismatch");
-//             return Err(MazeError::UnmatchedAccounts.into());
-//         }
-
-//         let mut f = f_ctx.borrow_mut()?;
-//         let proof_c = proof_c_ctx.take()?;
-
-//         let pvk = proof_type.verifying_key();
-//         let index = pvk.delta_g2_neg_pc.len() - 1;
-//         ell(&mut f, &pvk.delta_g2_neg_pc[index], &proof_c);
-        
-//         proof_c_ctx.close()?;
-//         Ok(FSM::FinalExponentInverse0(FinalExponentInverse0 {
-//             f: self.f,
-//         }))
-//     }
-// }
+        Ok(())
+    }
+}
