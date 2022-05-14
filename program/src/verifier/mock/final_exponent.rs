6ffbf5ed0d25d413393e86735324cfd4425d004e
Lone <gyl19930522@163.com>
2022-05-14 15:07:41 +0800

    update

--- program/src/verifier/mock/final_exponent.rs
+++ program/src/verifier/mock/final_exponent.rs
@@ -100,6 +100,8 @@ impl ExpByNegX1 {
             let y2 = y1.cyclotomic_square();
             let mut y3 = y2 * y1;
             y3.conjugate();
+            let _y4 = Fqk254::one();
+
             // finished
             Ok(())
         } else {
@@ -125,7 +127,9 @@ impl ExpByNegX2 {
             &self.y3_inv,
         );
         if finished {
-            let y5 = self.y4.cyclotomic_square();
+            let _y5 = self.y4.cyclotomic_square();
+            let _y6 = Fqk254::one();
+
             Ok(())
         } else {
             Ok(())
@@ -133,351 +137,52 @@ impl ExpByNegX2 {
     }
 }
 
-// #[derive(Clone, BorshSerialize, BorshDeserialize)]
-// pub struct FinalExponentMul2 {
-//     pub r: Fqk254, // Fqk254
-//     pub y0: Fqk254, // Fqk254
-// }
+pub struct FinalExponentMulStep3 {
+    pub index: u8,
+    pub y3: Fqk254,
+    pub y5: Fqk254,
+    pub y5_inv: Fqk254,
+    pub y6: Fqk254,
+}
 
-// impl FinalExponentMul2 {
-//     pub fn process(
-//         self,
-//     ) -> Result<(), ProgramError> {
-//         let y1 = self.y0.cyclotomic_square();
-//         let y2 = y1.cyclotomic_square();
-//         let y3 = y2 * y1;
+impl FinalExponentMulStep3 {
+    pub fn process(mut self) -> Result<(), ProgramError> {
+        let finished = exp_by_neg_x(
+            &mut self.index,
+            &mut self.y6,
+            &self.y5,
+            &self.y5_inv,
+        );
+        if finished {
+            self.y3.conjugate();
+            self.y6.conjugate();
 
-//         Ok(())
-//     }
-// }
+            Ok(())
+        } else {
+            Ok(())
+        }
+    }
+}
 
-// impl_fqk_mul_struct!(FinalExponentMul3, r, y1, y2);
+pub struct FinalExponentMulStep4 {
+    pub r: Fqk254,
+    pub y1: Fqk254,
+    pub y3: Fqk254,
+    pub y4: Fqk254,
+    pub y6: Fqk254,
+}
 
-// impl FinalExponentMul3 {
-//     pub fn process(
-//         self,
-//         y1_ctx: &Context<Fqk254>,
-//         y2_ctx: &Context<Fqk254>,
-//         y3_ctx: &Context<Fqk254>,
-//         y4_ctx: &Context<Fqk254>,
-//     ) -> Result<FSM, ProgramError> {
-//         if y1_ctx.pubkey() != &self.y1 {
-//             msg!("y1_ctx pubkey mismatch");
-//             return Err(MazeError::UnmatchedAccounts.into());
-//         }
-//         if y2_ctx.pubkey() != &self.y2 {
-//             msg!("y2_ctx pubkey mismatch");
-//             return Err(MazeError::UnmatchedAccounts.into());
-//         }
+impl FinalExponentMulStep4 {
+    pub fn process(self) -> Result<(), ProgramError> {
+        let y7 = self.y6 * &self.y4;
+        let y8 = y7 * &self.y3;
+        let _y9 = y8 * &self.y1;
+        let y10 = y8 * &self.y4;
+        let _y11 = y10 * &self.r;
 
-//         let y1 = y1_ctx.take()?;
-//         let y2 = y2_ctx.take()?;
-
-//         let y3 = y2 * y1;
-
-//         y2_ctx.erase()?;
-//         y3_ctx.fill(y3)?;
-//         y4_ctx.fill(Fqk254::one())?;
-//         Ok(FSM::FinalExponentMul4(FinalExponentMul4 {
-//             step: 1,
-//             index: 0,
-//             r: self.r,
-//             y1: self.y1,
-//             y3: *y3_ctx.pubkey(),
-//             y4: *y4_ctx.pubkey(),
-//         }))
-//     }
-// }
-
-// impl_exp_by_negx_struct!(FinalExponentMul4, y1, y3, y4);
-// impl_exp_by_neg_x!(FinalExponentMul4);
-
-// impl FinalExponentMul4 {
-//     pub fn process_0(
-//         mut self,
-//         y4_ctx: &Context<Fqk254>,
-//     ) -> Result<FSM, ProgramError> {
-//         if y4_ctx.pubkey() != &self.y4 {
-//             msg!("y4_ctx pubkey mismatch");
-//             return Err(MazeError::UnmatchedAccounts.into());
-//         }
-
-//         let mut res = y4_ctx.borrow_mut()?;
-
-//         res.square_in_place();
-
-//         self.step += 1;
-//         Ok(FSM::FinalExponentMul4(self))
-//     }
-
-//     pub fn process_1(
-//         self,
-//         y3_ctx: &Context<Fqk254>,
-//         y4_ctx: &Context<Fqk254>,
-//         y5_ctx: &Context<Fqk254>,
-//         y6_ctx: &Context<Fqk254>,
-//     ) -> Result<FSM, ProgramError> {
-//         if y3_ctx.pubkey() != &self.y3 {
-//             msg!("y3_ctx pubkey mismatch");
-//             return Err(MazeError::UnmatchedAccounts.into());
-//         }
-//         if y4_ctx.pubkey() != &self.y4 {
-//             msg!("y4_ctx pubkey mismatch");
-//             return Err(MazeError::UnmatchedAccounts.into());
-//         }
-
-//         let (s, is_self) = self.process_1_inner(y3_ctx, y4_ctx)?;
-//         if is_self {
-//             Ok(FSM::FinalExponentMul4(s))
-//         } else {
-//             let y5 = y4_ctx.borrow_mut()?.cyclotomic_square();
-
-//             y5_ctx.fill(y5)?;
-//             y6_ctx.fill(Fqk254::one())?;
-//             Ok(FSM::FinalExponentMul5(FinalExponentMul5 {
-//                 step: 1,
-//                 index: 0,
-//                 r: s.r,
-//                 y1: s.y1,
-//                 y3: s.y3,
-//                 y4: s.y4,
-//                 y5: *y5_ctx.pubkey(),
-//                 y6: *y6_ctx.pubkey(),
-//             }))
-//         }
-//     }
-// }
-
-// impl_exp_by_negx_struct!(FinalExponentMul5, y1, y3, y4, y5, y6);
-// impl_exp_by_neg_x!(FinalExponentMul5);
-
-// impl FinalExponentMul5 {
-//     pub fn process_0(
-//         mut self,
-//         y6_ctx: &Context<Fqk254>,
-//     ) -> Result<FSM, ProgramError> {
-//         if y6_ctx.pubkey() != &self.y4 {
-//             msg!("y6_ctx pubkey mismatch");
-//             return Err(MazeError::UnmatchedAccounts.into());
-//         }
-
-//         let mut res = y6_ctx.borrow_mut()?;
-
-//         res.square_in_place();
-
-//         self.step += 1;
-//         Ok(FSM::FinalExponentMul5(self))
-//     }
-
-//     pub fn process_1(
-//         self,
-//         y3_ctx: &Context<Fqk254>,
-//         y5_ctx: &Context<Fqk254>,
-//         y6_ctx: &Context<Fqk254>,
-//     ) -> Result<FSM, ProgramError> {
-//         if y3_ctx.pubkey() != &self.y3 {
-//             msg!("y3_ctx pubkey mismatch");
-//             return Err(MazeError::UnmatchedAccounts.into());
-//         }
-//         if y5_ctx.pubkey() != &self.y5 {
-//             msg!("y5_ctx pubkey mismatch");
-//             return Err(MazeError::UnmatchedAccounts.into());
-//         }
-//         if y6_ctx.pubkey() != &self.y6 {
-//             msg!("y6_ctx pubkey mismatch");
-//             return Err(MazeError::UnmatchedAccounts.into());
-//         }
-
-//         let (s, is_self) = self.process_1_inner(y5_ctx, y6_ctx)?;
-//         if is_self {
-//             Ok(FSM::FinalExponentMul5(s))
-//         } else {
-//             y3_ctx.borrow_mut()?.conjugate();
-//             y6_ctx.borrow_mut()?.conjugate();
-            
-//             y5_ctx.erase()?;
-//             Ok(FSM::FinalExponentMul6(FinalExponentMul6 {
-//                 r: s.r,
-//                 y1: s.y1,
-//                 y3: s.y3,
-//                 y4: s.y4,
-//                 y6: s.y6,
-//             }))
-//         }
-//     }
-// }
-
-// impl_fqk_mul_struct!(FinalExponentMul6, r, y1, y3, y4, y6);
-
-// impl FinalExponentMul6 {
-//     pub fn process(
-//         self,
-//         y4_ctx: &Context<Fqk254>,
-//         y6_ctx: &Context<Fqk254>,
-//         y7_ctx: &Context<Fqk254>,
-//     ) -> Result<FSM, ProgramError> {
-//         if y4_ctx.pubkey() != &self.y4 {
-//             msg!("y4_ctx pubkey mismatch");
-//             return Err(MazeError::UnmatchedAccounts.into());
-//         }
-//         if y6_ctx.pubkey() != &self.y6 {
-//             msg!("y6_ctx pubkey mismatch");
-//             return Err(MazeError::UnmatchedAccounts.into());
-//         }
-
-//         let y4 = y4_ctx.take()?;
-//         let y6 = y6_ctx.take()?;
-
-//         let y7 = y6 * y4;
-
-//         y6_ctx.erase()?;
-//         y7_ctx.fill(y7)?;
-//         Ok(FSM::FinalExponentMul7(FinalExponentMul7 {
-//             r: self.r,
-//             y1: self.y1,
-//             y3: self.y3,
-//             y4: self.y4,
-//             y7: *y7_ctx.pubkey(),
-//         }))
-//     }
-// }
-
-// impl_fqk_mul_struct!(FinalExponentMul7, r, y1, y3, y4, y7);
-
-// impl FinalExponentMul7 {
-//     pub fn process(
-//         self,
-//         y3_ctx: &Context<Fqk254>,
-//         y7_ctx: &Context<Fqk254>,
-//         y8_ctx: &Context<Fqk254>,
-//     ) -> Result<FSM, ProgramError> {
-//         if y3_ctx.pubkey() != &self.y3 {
-//             msg!("y3_ctx pubkey mismatch");
-//             return Err(MazeError::UnmatchedAccounts.into());
-//         }
-//         if y7_ctx.pubkey() != &self.y7 {
-//             msg!("y7_ctx pubkey mismatch");
-//             return Err(MazeError::UnmatchedAccounts.into());
-//         }
-
-//         let y3 = y3_ctx.take()?;
-//         let y7 = y7_ctx.take()?;
-
-//         let y8 = y7 * y3;
-
-//         y3_ctx.erase()?;
-//         y7_ctx.erase()?;
-//         y8_ctx.fill(y8)?;
-//         Ok(FSM::FinalExponentMul8(FinalExponentMul8 {
-//             r: self.r,
-//             y1: self.y1,
-//             y4: self.y4,
-//             y8: *y8_ctx.pubkey(),
-//         }))
-//     }
-// }
-
-// impl_fqk_mul_struct!(FinalExponentMul8, r, y1, y4, y8);
-
-// impl FinalExponentMul8 {
-//     pub fn process(
-//         self,
-//         y1_ctx: &Context<Fqk254>,
-//         y8_ctx: &Context<Fqk254>,
-//         y9_ctx: &Context<Fqk254>,
-//     ) -> Result<FSM, ProgramError> {
-//         if y1_ctx.pubkey() != &self.y1 {
-//             msg!("y1_ctx pubkey mismatch");
-//             return Err(MazeError::UnmatchedAccounts.into());
-//         }
-//         if y8_ctx.pubkey() != &self.y8 {
-//             msg!("y8_ctx pubkey mismatch");
-//             return Err(MazeError::UnmatchedAccounts.into());
-//         }
-
-//         let y1 = y1_ctx.take()?;
-//         let y8 = y8_ctx.take()?;
-
-//         let y9 = y8 * y1;
-
-//         y1_ctx.erase()?;
-//         y9_ctx.fill(y9)?;
-//         Ok(FSM::FinalExponentMul9(FinalExponentMul9 {
-//             r: self.r,
-//             y4: self.y4,
-//             y8: self.y8,
-//             y9: *y9_ctx.pubkey(),
-//         }))
-//     }
-// }
-
-// impl_fqk_mul_struct!(FinalExponentMul9, r, y4, y8, y9);
-
-// impl FinalExponentMul9 {
-//     pub fn process(
-//         self,
-//         y4_ctx: &Context<Fqk254>,
-//         y8_ctx: &Context<Fqk254>,
-//         y10_ctx: &Context<Fqk254>,
-//     ) -> Result<FSM, ProgramError> {
-//         if y4_ctx.pubkey() != &self.y4 {
-//             msg!("y4_ctx pubkey mismatch");
-//             return Err(MazeError::UnmatchedAccounts.into());
-//         }
-//         if y8_ctx.pubkey() != &self.y8 {
-//             msg!("y8_ctx pubkey mismatch");
-//             return Err(MazeError::UnmatchedAccounts.into());
-//         }
-
-//         let y4 = y4_ctx.take()?;
-//         let y8 = y8_ctx.take()?;
-
-//         let y10 = y8 * y4;
-
-//         y4_ctx.close()?;
-//         y10_ctx.fill(y10)?;
-//         Ok(FSM::FinalExponentMul10(FinalExponentMul10 {
-//             r: self.r,
-//             y8: self.y8,
-//             y9: self.y9,
-//             y10: *y10_ctx.pubkey(),
-//         }))
-//     }
-// }
-
-// impl_fqk_mul_struct!(FinalExponentMul10, r, y8, y9, y10);
-
-// impl FinalExponentMul10 {
-//     pub fn process(
-//         self,
-//         r_ctx: &Context<Fqk254>,
-//         y10_ctx: &Context<Fqk254>,
-//         y11_ctx: &Context<Fqk254>,
-//     ) -> Result<FSM, ProgramError> {
-//         if r_ctx.pubkey() != &self.r {
-//             msg!("r_ctx pubkey mismatch");
-//             return Err(MazeError::UnmatchedAccounts.into());
-//         }
-//         if y10_ctx.pubkey() != &self.y10 {
-//             msg!("y10_ctx pubkey mismatch");
-//             return Err(MazeError::UnmatchedAccounts.into());
-//         }
-
-//         let r = r_ctx.take()?;
-//         let y10 = y10_ctx.take()?;
-
-//         let y11 = y10 * r;
-
-//         y10_ctx.erase()?;
-//         y11_ctx.fill(y11)?;
-//         Ok(FSM::FinalExponentMul11(FinalExponentMul11 {
-//             r: self.r,
-//             y8: self.y8,
-//             y9: self.y9,
-//             y11: *y11_ctx.pubkey(),
-//         }))
-//     }
-// }
+        Ok(())
+    }
+}
 
 // impl_fqk_mul_struct!(FinalExponentMul11, r, y8, y9, y11);
 
