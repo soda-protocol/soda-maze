a15a8e225bd473b34dc9f7feff93f1afa7219876
Lone <gyl19930522@163.com>
2022-05-14 16:54:52 +0800

    update

--- program/src/processor1.rs
+++ program/src/processor1.rs
@@ -238,7 +238,7 @@ mod tests {
             &[Instruction {
                 program_id: id(),
                 accounts: vec![],
-                data: vec![56],
+                data: vec![2, 38],
             }],
             Some(&user.pubkey()),
             &[&user],
