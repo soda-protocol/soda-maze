23c8edbd05e3f05a6c3c4dd240f69ca46a427257
Lone <gyl19930522@163.com>
2022-05-29 21:42:12 +0800

    update

--- program/src/verifier/miller_loop.rs
+++ program/src/verifier/miller_loop.rs
@@ -54,12 +54,12 @@ impl MillerLoop {
     pub fn process(mut self, pvk: &PreparedVerifyingKey) -> Program {
         let ate_loop_count_inv = <BnParameters as Bn>::ATE_LOOP_COUNT_INV;
 
-        const MAX_UINTS: usize = 1350000;
+        const MAX_UNITS: usize = 1350000;
         let mut used_units = 0;
         loop {
             match self.step {
                 ComputeStep::Step0 => {
-                    if used_units + 100000 >= MAX_UINTS {
+                    if used_units + 100000 >= MAX_UNITS {
                         break;
                     }
                     self.f.square_in_place();
@@ -67,7 +67,7 @@ impl MillerLoop {
                     self.step = ComputeStep::Step1;
                 }
                 ComputeStep::Step1 => {
-                    if used_units + 155000 >= MAX_UINTS {
+                    if used_units + 155000 >= MAX_UNITS {
                         break;
                     }
                     let coeff = doubling_step(&mut self.r, FQ_TWO_INV);
@@ -76,7 +76,7 @@ impl MillerLoop {
                     self.step = ComputeStep::Step2;
                 }
                 ComputeStep::Step2 => {
-                    if used_units + 90000 >= MAX_UINTS {
+                    if used_units + 90000 >= MAX_UNITS {
                         break;
                     }
                     ell(&mut self.f, &pvk.gamma_g2_neg_pc[self.coeff_index as usize], &self.prepared_input);
@@ -84,7 +84,7 @@ impl MillerLoop {
                     self.step = ComputeStep::Step3;
                 }
                 ComputeStep::Step3 => {
-                    if used_units + 90000 >= MAX_UINTS {
+                    if used_units + 90000 >= MAX_UNITS {
                         break;
                     }
                     ell(&mut self.f, &pvk.delta_g2_neg_pc[self.coeff_index as usize], &self.proof_c);
@@ -94,10 +94,9 @@ impl MillerLoop {
                 }
                 ComputeStep::Step4 => {
                     let bit = ate_loop_count_inv[self.ate_index as usize];
-                    self.ate_index += 1;
                     match bit {
                         1 => {
-                            if used_units + 155000 >= MAX_UINTS {
+                            if used_units + 155000 >= MAX_UNITS {
                                 break;
                             }
                             let coeff = addition_step(&mut self.r, &self.proof_b);
@@ -106,7 +105,7 @@ impl MillerLoop {
                             self.step = ComputeStep::Step5;
                         },
                         -1 => {
-                            if used_units + 155000 >= MAX_UINTS {
+                            if used_units + 155000 >= MAX_UNITS {
                                 break;
                             }
                             let coeff = addition_step(&mut self.r, &self.proof_b_neg);
@@ -115,7 +114,7 @@ impl MillerLoop {
                             self.step = ComputeStep::Step5;
                         },
                         _ => {
-                            if (self.ate_index as usize) >= ate_loop_count_inv.len() {
+                            if (self.ate_index as usize) >= ate_loop_count_inv.len() - 1 {
                                 // in Finalize
                                 return Program::MillerLoopFinalize(MillerLoopFinalize {
                                     coeff_index: self.coeff_index,
@@ -131,9 +130,10 @@ impl MillerLoop {
                             }
                         },
                     };
+                    self.ate_index += 1;
                 }
                 ComputeStep::Step5 => {
-                    if used_units + 90000 >= MAX_UINTS {
+                    if used_units + 90000 >= MAX_UNITS {
                         break;
                     }
                     ell(&mut self.f, &pvk.gamma_g2_neg_pc[self.coeff_index as usize], &self.prepared_input);
@@ -141,14 +141,14 @@ impl MillerLoop {
                     self.step = ComputeStep::Step6;
                 }
                 ComputeStep::Step6 => {
-                    if used_units + 90000 >= MAX_UINTS {
+                    if used_units + 90000 >= MAX_UNITS {
                         break;
                     }
                     ell(&mut self.f, &pvk.delta_g2_neg_pc[self.coeff_index as usize], &self.proof_c);
                     self.coeff_index += 1;
                     used_units += 90000;
 
-                    if (self.ate_index as usize) >= ate_loop_count_inv.len() {
+                    if (self.ate_index as usize) >= ate_loop_count_inv.len() - 1 {
                         // in Finalize
                         return Program::MillerLoopFinalize(MillerLoopFinalize {
                             coeff_index: self.coeff_index,
@@ -208,5 +208,161 @@ impl MillerLoopFinalize {
         Program::FinalExponentEasyPart(FinalExponentEasyPart {
             f: self.f,
         })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::ops::Neg;
+    use num_traits::One;
+
+    use crate::params::{bn::*, proof::{PreparedVerifyingKey, ProofType}};
+    use crate::verifier::{ProofA, ProofB, ProofC, program::Program};
+    use crate::bn::BigInteger256 as BigInteger;
+    use super::{MillerLoop, ComputeStep};
+
+    const PVK: &PreparedVerifyingKey = ProofType::Deposit.pvk();
+
+    fn get_proof_data() -> (ProofA, ProofB, ProofC) {
+        let proof_a = ProofA::new_const(
+            Fq::new(BigInteger::new([3750417186220724512, 3978078781434640716, 15163791108043952614, 2453596515077279990])),
+            Fq::new(BigInteger::new([5354853820532153524, 8883007908664368954, 470161243035897903, 1359038641147964963])),
+            false
+        );
+        let proof_b = ProofB::new_const(
+            Fq2::new_const(
+                Fq::new(BigInteger::new([12118601996045181130, 896706683785346415, 4709517509465227924, 1819241630933245065])),
+                Fq::new(BigInteger::new([16349181015735361827, 4843110160248729036, 17714835083434401718, 2754712195795085383])),
+            ),
+            Fq2::new_const(
+                Fq::new(BigInteger::new([3167422245359854874, 15117403505212976980, 14561078193533486427, 992932037830603307])),
+                Fq::new(BigInteger::new([10453996433908490996, 4951364747808814581, 1077088453432665796, 3244165116791247838])),
+            ),
+            false
+        );
+        let proof_c = ProofC::new_const(
+            Fq::new(BigInteger::new([6745960168647187300, 7304089792560402287, 5467772039812183716, 1531927553351135845])),
+            Fq::new(BigInteger::new([2914263778726088111, 9472631376659388131, 16215105594981982902, 939471742250680668])),
+            false
+        );
+
+        (proof_a, proof_b, proof_c)
+    }
+
+    fn get_deposit_verifying_program(proof_a: ProofA, proof_b: ProofB, proof_c: ProofC) -> (G1Affine254, Program) {
+        let proof_b_neg = proof_b.neg();
+
+        let r = G2HomProjective254 {
+            x: proof_b.x,
+            y: proof_b.y,
+            z: Fq2::one(),
+        };
+        let prepared_input = G1Affine254::new(
+            Fq::new(BigInteger::new([15366109605146242524, 8248000168982217248, 10784397664560829633, 3448109755660551539])),
+            Fq::new(BigInteger::new([10288887847322244487, 17147661464723726713, 11641908225792268002, 290404619346461690])),
+            false
+        );
+
+        (prepared_input.clone(), Program::MillerLoop(MillerLoop {
+            step: ComputeStep::Step0,
+            ate_index: 0,
+            coeff_index: 0,
+            f: Box::new(Fqk254::one()),
+            r: Box::new(r),
+            prepared_input: Box::new(prepared_input),
+            proof_a: Box::new(proof_a),
+            proof_b: Box::new(proof_b),
+            proof_b_neg: Box::new(proof_b_neg),
+            proof_c: Box::new(proof_c),
+        }))
+    }
+
+    fn transform_biginteger(i: BigInteger) -> ark_ff::BigInteger256 {
+        use ark_ff::BigInteger256;
+        BigInteger256::new(i.0)
+    }
+
+    fn transform_g1_affine(g: &G1Affine254) -> ark_bn254::G1Affine {
+        use ark_bn254::{Fq, G1Affine};
+
+        G1Affine::new(
+            Fq::new(transform_biginteger(g.x.0)),
+            Fq::new(transform_biginteger(g.y.0)),
+            g.infinity,
+        )
+    }
+
+    fn transform_g2_affine(g: &G2Affine254) -> ark_bn254::G2Affine {
+        use ark_bn254::{Fq, Fq2, G2Affine};
+
+        G2Affine::new(
+            Fq2::new(
+                Fq::new(transform_biginteger(g.x.c0.0)),
+                Fq::new(transform_biginteger(g.x.c1.0)),
+            ),
+            Fq2::new(
+                Fq::new(transform_biginteger(g.y.c0.0)),
+                Fq::new(transform_biginteger(g.y.c1.0)),
+            ),
+            g.infinity,
+        )
+    }
+
+    fn transform_g2_prepared(g: &[EllCoeffFq2]) -> ark_ec::bn::G2Prepared<ark_bn254::Parameters> {
+        use ark_bn254::{Fq, Fq2, Parameters};
+        use ark_ec::bn::G2Prepared;
+
+        let ell_coeffs = g.iter().map(|(g1, g2, g3)| {
+            (
+                Fq2::new(Fq::new(transform_biginteger(g1.c0.0)), Fq::new(transform_biginteger(g1.c1.0))),
+                Fq2::new(Fq::new(transform_biginteger(g2.c0.0)), Fq::new(transform_biginteger(g2.c1.0))),
+                Fq2::new(Fq::new(transform_biginteger(g3.c0.0)), Fq::new(transform_biginteger(g3.c1.0))),
+            )
+        }).collect::<Vec<_>>();
+
+        G2Prepared::<Parameters> {
+            ell_coeffs,
+            infinity: false,
+        }
+    }
+
+    #[test]
+    fn test_miller_loop() {
+        let (proof_a, proof_b, proof_c) = get_proof_data();
+        let (prepared_inputs, mut program) = get_deposit_verifying_program(proof_a.clone(), proof_b.clone(), proof_c.clone());
+
+        loop {
+            program = program.process(PVK);
+            if let Program::FinalExponentEasyPart(e) = &program {
+                println!("out {:?}", e.f.c0.c0.c0.0.0);
+                break;
+            }
+        }
+
+        {
+            use ark_ec::PairingEngine;
+            use ark_bn254::Bn254;
+
+            let prepared_inputs = transform_g1_affine(&prepared_inputs);
+            let proof_a = transform_g1_affine(&proof_a);
+            let proof_b = transform_g2_affine(&proof_b);
+            let proof_c = transform_g1_affine(&proof_c);
+
+            let pvk_gamma_g2_neg_pc = transform_g2_prepared(&PVK.gamma_g2_neg_pc);
+            let pvk_delta_g2_neg_pc = transform_g2_prepared(&PVK.delta_g2_neg_pc);
+
+            let out = Bn254::miller_loop(
+                [
+                    (proof_a.into(), proof_b.into()),
+                    (
+                        prepared_inputs.into(),
+                        pvk_gamma_g2_neg_pc,
+                    ),
+                    (proof_c.into(), pvk_delta_g2_neg_pc),
+                ].iter()
+            );
+
+            println!("out {:?}", out);
+        }
     }
 }
--- program/src/verifier/prepare_inputs.rs
+++ program/src/verifier/prepare_inputs.rs
@@ -85,69 +85,155 @@ impl PrepareInputs {
 
 #[cfg(test)]
 mod tests {
-    use crate::{params::{bn::{G1Affine254, G2Affine254, G1Projective254}, proof::{PreparedVerifyingKey, ProofType}}, verifier::{ProofA, ProofB, ProofC, program::Program}};
-    use crate::params::bn::{Fq, Fq2};
+    use solana_program::pubkey::Pubkey;
+
+    use crate::params::{bn::{G1Affine254, G1Projective254}, proof::{PreparedVerifyingKey, ProofType}};
+    use crate::{params::bn::{Fq, Fq2}, verifier::{ProofA, ProofB, ProofC, program::Program}};
     use crate::bn::BigInteger256 as BigInteger;
-
-    use super::PrepareInputs;
-
-    const PROOF_A: ProofA = G1Affine254::new_const(
-        Fq::new(BigInteger::new([14715620368662735844, 9563436648438579353, 9817845158629706665, 2420889558595263392])),
-        Fq::new(BigInteger::new([8640892419674201321, 14834230856296141528, 4198848546444402927, 1517119377864516134])),
-        false,
-    );
-    
-    const PROOF_B: ProofB = G2Affine254::new_const(
-        Fq2::new_const(
-            Fq::new(BigInteger::new([14384816041077872766, 431448166635449345, 6321897284235301150, 2191027455511027545])),
-            Fq::new(BigInteger::new([4791893780199645830, 13020716387556337386, 12915032691238673322, 2866902253618994548])),
-        ),
-        Fq2::new_const(
-            Fq::new(BigInteger::new([2204364260910044889, 4961323307537146896, 3192016866730518327, 1801533657434404900])),
-            Fq::new(BigInteger::new([13208303890985533178, 12442437710149681723, 9219358705006067983, 3191371954673554778])),
-        ),
-        false,
-    );
-    
-    const PROOF_C: ProofC = G1Affine254::new_const(
-        Fq::new(BigInteger::new([5823303549099682051, 11298647609364880259, 17539675314511186284, 556302735522023958])),
-        Fq::new(BigInteger::new([2083577888616351182, 10916945937534065039, 1520021691683278293, 2748969749429754277])),
-        false,
-    );
-
-    const G1_PROJECTIVE_VALUE: G1Projective254 = G1Projective254::new_const(
-        Fq::new(BigInteger::new([8702585202244274910, 9214718725403065568, 17690655619678158896, 1222195394398354666])),
-        Fq::new(BigInteger::new([3439699351422384141, 18051431940401055444, 13194437363659758174, 2607686238957372954])),
-        Fq::new(BigInteger::new([15230403791020821917, 754611498739239741, 7381016538464732716, 1011752739694698287])),
-    );
-
-    const PREPARE_INPUT: BigInteger = BigInteger::new([14384816041077872766, 431448166635449345, 6321897284235301150, 2191027455511027545]);
+    use crate::core::{VanillaData, deposit::DepositVanillaData};
 
     const PVK: &PreparedVerifyingKey = ProofType::Deposit.pvk();
 
+    fn get_deposit_verifying_program() -> (Box<Vec<BigInteger>>, Program) {
+        let deposit_amount = 100;
+        let leaf_index = 0;
+        let leaf = BigInteger::new([3542236639209175990, 16910505828447755939, 15985469206914547775, 2949265978052157896]);
+        let prev_root = BigInteger::new([15731961227988085298, 1152253436246937880, 10067708109528847282, 2453386543983348226]);
+        let updating_nodes = vec![
+            BigInteger::new([15532250321868931685, 772932733899588440, 12868310124187153130, 438462560823777455]),
+            BigInteger::new([11847340026267790185, 10820144684227279182, 3897917803026447095, 1211025166583652450]),
+            BigInteger::new([13871474726796312921, 2045639111475989628, 12481963867359042585, 1654720166251331239]),
+            BigInteger::new([7522360132978259117, 14771120575066486403, 10596590224358807127, 3157651300534472347]),
+            BigInteger::new([7507876248263243529, 5715413346482742507, 12957876777088811968, 2510703228340708577]),
+            BigInteger::new([133315792692492865, 15293309774567381972, 14334463947285336696, 1723563495644442414]),
+            BigInteger::new([1928085400153529539, 1698449431575688062, 5445397574952319768, 1143000330999000263]),
+            BigInteger::new([12979161123112243949, 10519306232363901245, 7410924906293113533, 441442420902499555]),
+            BigInteger::new([13623320368263364327, 8828774309128316872, 10934886998453446221, 1177057120107296621]),
+            BigInteger::new([17522708475201759282, 412032152261673971, 16434968819987934970, 57110405472686226]),
+            BigInteger::new([12273231996521786577, 8864960514473101270, 7255808797058973254, 2024561412595145600]),
+            BigInteger::new([8041353708081998109, 4408055454208809679, 7467631578407169415, 174481746946129969]),
+            BigInteger::new([15759627427490212038, 2342726286939514839, 3188233064319415482, 206053989429984523]),
+            BigInteger::new([3862288674501080527, 4414699166683294138, 7867804525257430666, 2497191292622968527]),
+            BigInteger::new([18111671256300872493, 9560658010795795413, 11128501249746692797, 3218688909781081982]),
+            BigInteger::new([11621730998744754762, 6316575933548212603, 1290755564488270042, 1649482600435840483]),
+            BigInteger::new([14372000062670910161, 12363655746768868914, 2239893263450009019, 1148238205359365334]),
+            BigInteger::new([7047592107069037456, 15046063210205594002, 8658295702548938809, 1627711097087034838]),
+            BigInteger::new([778320942488412995, 1973661381734835396, 12584060032923075028, 1668508506270919639]),
+            BigInteger::new([15541703251344978826, 10978453290527186359, 10233791787923230785, 179983619992156155]),
+            BigInteger::new([1403833390002913823, 14192997889125093942, 2057500286915250275, 2091709604487301396]),
+        ];
+        let commitment = vec![
+            BigInteger::new([16722997434160713798, 11403452488286244511, 18318868681545149281, 21754274364414989]),
+            BigInteger::new([11611806235245355479, 5424040539426569871, 7513338721988059883, 35367902979566062]),
+            BigInteger::new([220681830571333505, 13034651635228622148, 14955611269817919911, 36862314553737607]),
+            BigInteger::new([3646867184726427713, 5600318523685585750, 7642679702590823310, 40280276519090518]),
+            BigInteger::new([2053746354640544201, 5271193340300995188, 15781609477155030499, 33238881268910210]),
+            BigInteger::new([4886134654328406654, 12634074070563300144, 17891432476597062324, 71955938858561633]),
+            BigInteger::new([2559727239711704880, 6392075204380784424, 12055047205046880238, 9598153984261654]),
+            BigInteger::new([8099615302498656019, 17681822004623220591, 4278720356088691622, 20549192218165015]),
+            BigInteger::new([1479306651680053975, 16970454663387229825, 1219617339513386804, 9996197586358739]),
+            BigInteger::new([12513940028146829811, 16771911556576546385, 12887667978113417874, 36027991776611425]),
+            BigInteger::new([18410058132998784786, 13401630289159459721, 14914310748430415085, 18313255534332353]),
+            BigInteger::new([5826441929749290616, 11335202586746830014, 10903293645248433631, 36117579937827459]),
+        ];
+        let proof_a = ProofA::new_const(
+            Fq::new(BigInteger::new([3750417186220724512, 3978078781434640716, 15163791108043952614, 2453596515077279990])),
+            Fq::new(BigInteger::new([5354853820532153524, 8883007908664368954, 470161243035897903, 1359038641147964963])),
+            false
+        );
+        let proof_b = ProofB::new_const(
+            Fq2::new_const(
+                Fq::new(BigInteger::new([12118601996045181130, 896706683785346415, 4709517509465227924, 1819241630933245065])),
+                Fq::new(BigInteger::new([16349181015735361827, 4843110160248729036, 17714835083434401718, 2754712195795085383])),
+            ),
+            Fq2::new_const(
+                Fq::new(BigInteger::new([3167422245359854874, 15117403505212976980, 14561078193533486427, 992932037830603307])),
+                Fq::new(BigInteger::new([10453996433908490996, 4951364747808814581, 1077088453432665796, 3244165116791247838])),
+            ),
+            false
+        );
+        let proof_c = ProofC::new_const(
+            Fq::new(BigInteger::new([6745960168647187300, 7304089792560402287, 5467772039812183716, 1531927553351135845])),
+            Fq::new(BigInteger::new([2914263778726088111, 9472631376659388131, 16215105594981982902, 939471742250680668])),
+            false
+        );
+
+        let mut deposit_data = DepositVanillaData::new(
+            deposit_amount,
+            leaf_index,
+            leaf,
+            prev_root,
+            Box::new(updating_nodes),
+        );
+        deposit_data.fill_commitment(Box::new(commitment)).unwrap();
+
+        let public_inputs = deposit_data.clone().to_public_inputs();
+        let verifier = deposit_data.to_verifier(
+            Pubkey::default(),
+            Box::new(proof_a),
+            Box::new(proof_b),
+            Box::new(proof_c),
+        );
+
+        (public_inputs, verifier.program)
+    }
+
+    fn transform_biginteger(i: BigInteger) -> ark_ff::BigInteger256 {
+        use ark_ff::BigInteger256;
+
+        BigInteger256::new(i.0)
+    }
+
+    fn transform_g1_affine(g: &G1Affine254) -> ark_bn254::G1Affine {
+        use ark_ff::BigInteger256;
+        use ark_bn254::{Fq, G1Affine};
+
+        G1Affine::new(
+            Fq::new(BigInteger256::new(g.x.0.0)),
+            Fq::new(BigInteger256::new(g.y.0.0)),
+            g.infinity,
+        )
+    }
+
+    fn transform_g1_projective(g: &G1Projective254) -> ark_bn254::G1Projective {
+        use ark_ff::BigInteger256;
+        use ark_bn254::{Fq, G1Projective};
+
+        G1Projective::new(
+            Fq::new(BigInteger256::new(g.x.0.0)),
+            Fq::new(BigInteger256::new(g.y.0.0)),
+            Fq::new(BigInteger256::new(g.z.0.0)),
+        )
+    }
+
     #[test]
     fn test_prepare_inputs() {
-        let prepare_inputs = PrepareInputs {
-            input_index: 0,
-            bit_index: 0,
-            public_inputs: Box::new(vec![PREPARE_INPUT]),
-            g_ic: Box::new(G1_PROJECTIVE_VALUE),
-            tmp: Box::new(G1_PROJECTIVE_VALUE),
-            proof_a: Box::new(PROOF_A),
-            proof_b: Box::new(PROOF_B),
-            proof_c: Box::new(PROOF_C),
-        };
+        let (public_inputs, mut program) = get_deposit_verifying_program();
 
-        let program = prepare_inputs.process(PVK);
+        loop {
+            program = program.process(PVK);
+            if let Program::MillerLoop(ml) = &program {
+                println!("prepared_input_x {:?}", &ml.prepared_input.x.0.0);
+                println!("prepared_input_y {:?}", &ml.prepared_input.y.0.0);
+                break;
+            }
+        }
 
-        match program {
-            Program::PrepareInputs(pi) => {
-                println!("{:?}", pi.bit_index);
-            }
-            Program::MillerLoop(mi) => {
-                println!("{:?}", mi.coeff_index);
-            }
-            _ => unreachable!(),
+        {
+            use std::ops::AddAssign;
+            use ark_ec::{AffineCurve, ProjectiveCurve};
+
+            let mut g_ic = transform_g1_projective(PVK.g_ic_init);
+            public_inputs.into_iter().zip(PVK.gamma_abc_g1).for_each(|(i, b)| {
+                let i = transform_biginteger(i);
+                let b = transform_g1_affine(b);
+                g_ic.add_assign(&b.mul(i));
+            });
+
+            let public_input = g_ic.into_affine();
+
+            println!("public_input_x {:?}", public_input.x.0.0);
+            println!("public_input_y {:?}", public_input.y.0.0);
         }
     }
 }
